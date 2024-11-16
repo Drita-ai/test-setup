38f93a3d675bea4600d2c4ecc9bcf735089c0ad6
alok <alok.kr300@gmail.com>
2024-11-16 11:49:34 +0530

    added tests for checking the password

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@jest/types";
 
 const baseDir = "<rootDir>/src/app/pass_checker";
-const baseTestDir = "<rootDir>/src/app/pass_checker";
+const baseTestDir = "<rootDir>/src/test/pass_checker";
 
 const config: Config.InitialOptions = {
   preset: "ts-jest",
