5854ab33491137b1f67053b56a67903191284003
alok <alok.kr300@gmail.com>
2024-11-16 12:58:38 +0530

    refactored PasswordChecker

--- src/app/pass_checker/PasswordChecker.ts
+++ src/app/pass_checker/PasswordChecker.ts
@@ -13,15 +13,26 @@ export class PasswordChecker {
   public checkPassword(password: string): CheckResult {
     const reasons: PasswordErrors[] = [];
 
+    this.checkForLength(password, reasons);
+    this.checkForUpperCase(password, reasons);
+    this.checkForLowerCase(password, reasons);
+
+    return { valid: reasons.length > 0 ? false : true, reasons };
+  }
+
+  private checkForLength(password: string, reasons: PasswordErrors[]) {
     if (password.length < 8) {
       reasons.push(PasswordErrors.SHORT);
     }
+  }
+  private checkForUpperCase(password: string, reasons: PasswordErrors[]) {
     if (password == password.toLowerCase()) {
       reasons.push(PasswordErrors.NO_UPPER_CASE);
     }
+  }
+  private checkForLowerCase(password: string, reasons: PasswordErrors[]) {
     if (password == password.toUpperCase()) {
       reasons.push(PasswordErrors.NO_LOWER_CASE);
     }
-    return { valid: reasons.length > 0 ? false : true, reasons };
   }
 }
