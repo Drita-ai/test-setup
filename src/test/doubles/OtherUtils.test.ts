b3fb89ca3316ebb1158b357c336613dcc98a4cd4
alok <alok.kr300@gmail.com>
2024-11-17 14:53:54 +0530

    implement: jest mocks in 2 different ways

--- src/test/doubles/OtherUtils.test.ts
+++ src/test/doubles/OtherUtils.test.ts
@@ -4,6 +4,61 @@ import {
 } from "../../app/doubles/OtherUtils";
 
 describe("OtherUtils test suite", () => {
+  describe("Tracking callbacks with JEST mocks", () => {
+    // Callback Mock
+    const callBackMock = jest.fn();
+
+    // reset mocks
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("calls callback for invalid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("", callBackMock);
+      expect(actual).toBeUndefined();
+      expect(callBackMock).toBeCalledWith("Invalid argument!");
+      expect(callBackMock).toBeCalledTimes(1);
+    });
+
+    it("calls callback for valid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("abc", callBackMock);
+      expect(actual).toBe("ABC");
+      expect(callBackMock).toBeCalledWith("called function with abc");
+      expect(callBackMock).toBeCalledTimes(1);
+    });
+  });
+
+  describe("Tracking callbacks", () => {
+    let cbArgs = [];
+    let timesCalled = 0;
+
+    // We want to track the way it is called and with what argument
+    function callBackMock(arg: string) {
+      cbArgs.push(arg);
+      timesCalled++;
+    }
+
+    afterEach(() => {
+      // clearing tracking fields
+      cbArgs = [];
+      timesCalled = 0;
+    });
+
+    it("calls callback for invalid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("", callBackMock);
+      expect(actual).toBeUndefined();
+      expect(cbArgs).toContain("Invalid argument!");
+      expect(timesCalled).toBe(1);
+    });
+
+    it("calls callback for valid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("abc", callBackMock);
+      expect(actual).toBe("ABC");
+      expect(cbArgs).toContain("called function with abc");
+      expect(timesCalled).toBe(1);
+    });
+  });
+
   it("ToUpperCase - calls callback for invalid argument", () => {
     const actual = toUpperCaseWithCb("", () => {});
     expect(actual).toBeUndefined();
